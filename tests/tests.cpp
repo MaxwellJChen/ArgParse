8da41f07bd88ac73936782c74e881ad30c94b8a3
MaxwellJChen <maxwelljchen1@gmail.com>
2025-05-01 21:52:11 -0500

    added additional tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,23 +4,6 @@
 #include <sstream>
 #include "../argparse.h"
 
-
-void bar(int x, float y, double z) {
-    std::cout<<x + y * z<<std::endl;
-}
-
-void baz() {
-    std::cout<<"baz"<<std::endl;
-}
-
-struct test_t {
-    int a;
-};
-
-void test(test_t t) {
-    std::cout<<t.a<<std::endl;
-}
-
 class ArgParseTests : public ::testing::Test {
 protected:
     std::stringstream input_buffer;
@@ -60,12 +43,11 @@ TEST_F(ArgParseTests, SingleArgumentTest) {
 }
 
 TEST_F(ArgParseTests, MultiArgumentTest) {
-    ArgParse ap;
-
     void (*func)(int, float, double) = [](int x, float y, double z) {
         std::cout<<x + y * z<<std::endl;
     };
 
+    ArgParse ap;
     ap.add_command<3, int, float, double>({"bar", "baz", "foo"}, func);
 
     int argc = 7;
@@ -73,4 +55,63 @@ TEST_F(ArgParseTests, MultiArgumentTest) {
     ap.execute_command(argc, argv);
 
     EXPECT_EQ(output_buffer.str(), "385.166\n");
+}
+
+TEST_F(ArgParseTests, CustomTypeTest) {
+    struct test_t {
+        int a;
+    };
+
+    void (*func)(test_t) = [](test_t t) {
+        std::cout<<t.a * 2<<std::endl;
+    };
+
+    ArgParse ap;
+    ap.add_conversion<test_t>([](std::string s) {
+        return test_t { stoi(s) };
+    });
+    ap.add_command<1, test_t>({"test"}, func);
+
+    int argc = 3;
+    char* argv[] = {"argparse", "test", "500"};
+    ap.execute_command(argc, argv);
+
+    EXPECT_EQ(output_buffer.str(), "1000\n");
+}
+
+TEST_F(ArgParseTests, AliasTest) {
+    ArgParse ap;
+
+    void (*func)(int) = [](int x) {
+        std::cout<<x * 2<<std::endl;
+    };
+
+    ap.add_command<1, int>({"bar", "baz", "foo"}, func);
+    ap.add_alias({"bar"}, "b");
+    ap.add_alias({"bar", "baz"}, "b");
+    ap.add_alias({"bar", "baz", "foo"}, "f");
+
+
+    int argc = 5;
+    char* argv[] = {"argparse", "b", "b", "f", "500"};
+    ap.execute_command(argc, argv);
+
+    EXPECT_EQ(output_buffer.str(), "1000\n");
+}
+
+TEST_F(ArgParseTests, NoArgumentsTest) {
+    ArgParse ap;
+
+    void (*func)(void) = []() {
+        std::cout<<"test"<<std::endl;
+    };
+
+    ap.add_command<0>({"bar", "baz", "foo"}, func);
+
+
+    int argc = 4;
+    char* argv[] = {"argparse", "bar", "baz", "foo"};
+    ap.execute_command(argc, argv);
+
+    EXPECT_EQ(output_buffer.str(), "test\n");
 }
